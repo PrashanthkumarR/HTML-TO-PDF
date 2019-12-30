3c1af32e487a78ff4d21fae007741f6ab31a3a0d
Abdul Mohaimin <abdul.mohaimin@zibtek.in>
2019-12-30 09:43:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser');
+const pdf = require('./public/createpdf')
+const cors =  require('cors')
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './invoice.html'));
+});
+
+
+app.post('/generatepdf', function (req, res) {
+    try { 
+        pdf.generatePdf().then((result)=>{
+            res.send(req.headers.origin + 'invoicesample.pdf')
+            
+        }).catch((e) => {
+            Promise.reject(e)
+        })
+    } 
+    catch (e) {
+     console.log('something went wrong' , e)
+    }
+});
+
+
+app.listen(3000, () => console.log('app listening on port 3000!'));
