43206e262788d4f7abd4ee192395231e4def3874
Abdul Mohaimin <abdul.mohaimin@zibtek.in>
2019-12-30 10:14:11 +0530

    application starter listen

--- index.js
+++ index.js
@@ -35,4 +35,4 @@ app.post('/generatepdf', function (req, res) {
 });
 
 
-app.listen(3000, () => console.log('app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('app listening on port 3000!'));
